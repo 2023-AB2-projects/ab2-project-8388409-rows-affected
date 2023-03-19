0c882b4a0ac175a78c31577116892ff725c1f11f
kisgyorgyzoltan <zolee2002@gmail.com>
2023-03-19 18:08:53 +0200

    alap catalog

--- ABPROJEKT/src/Host.java
+++ ABPROJEKT/src/Host.java
@@ -1,13 +1,93 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class Host {
-    public void Create(){
+    JSONObject catalog = new JSONObject();
+    public void Create() {
         System.out.println("Starting server...");
+
+        JSONObject jsonObject = new JSONObject();
+
+        // Creating the innermost JSONObject
+        JSONObject indexAttr = new JSONObject();
+        indexAttr.put("IAttribute", "SpecName");
+
+        // Creating the innermost JSONArray
+        JSONArray indexFileArray = new JSONArray();
+        JSONObject indexFile1 = new JSONObject();
+        indexFile1.put("IndexAttributes", indexAttr);
+        indexFileArray.add(indexFile1);
+
+        // Creating the innermost JSONObject
+        JSONObject uniqueAttr = new JSONObject();
+        uniqueAttr.put("UniqueAttribute", "SpecName");
+
+        // Creating the innermost JSONArray
+        JSONArray tableArray = new JSONArray();
+        JSONObject table1 = new JSONObject();
+        table1.put("Structure", new JSONObject().put("Attribute", new JSONArray()));
+        table1.put("primaryKey", new JSONObject().put("pkAttribute", "Specid"));
+        table1.put("uniqueKeys", uniqueAttr);
+        table1.put("IndexFiles", new JSONObject().put("IndexFile", indexFileArray.get(0)));
+        tableArray.add(table1);
+
+        // Creating the innermost JSONObject
+        JSONObject ref = new JSONObject();
+        ref.put("refTable", "Specialization");
+        ref.put("refAttribute", "Specid");
+        JSONObject foreignKey = new JSONObject();
+        foreignKey.put("fkAttribute", "Specid");
+        foreignKey.put("references", ref);
+
+        // Creating the innermost JSONArray
+        JSONObject table2 = new JSONObject();
+        table2.put("Structure", new JSONObject().put("Attribute", new JSONArray()));
+        table2.put("primaryKey", new JSONObject().put("pkAttribute", "Groupid"));
+        table2.put("foreignKeys", foreignKey);
+        table2.put("IndexFiles", new JSONObject().put("IndexFile", indexFileArray.get(0)));
+        tableArray.add(table2);
+
+        // Creating the innermost JSONObject
+        JSONObject tables = new JSONObject();
+        tables.put("Table", tableArray);
+
+        // Creating the innermost JSONObject
+        JSONObject database = new JSONObject();
+        database.put("Tables", tables);
+
+        // Creating the outermost JSONObject
+        JSONObject databases = new JSONObject();
+        databases.put("DataBase", database);
+        jsonObject.put("Databases", databases);
+
+
+        try {
+            Reader reader = new FileReader("Catalog.json");
+            catalog = (JSONObject) new JSONParser().parse(reader);
+            // if empty, initialize
+            if (catalog.isEmpty()) {
+                reader.close();
+                FileWriter fileWriter = new FileWriter("Catalog.json");
+                fileWriter.write(jsonObject.toJSONString());
+                fileWriter.close();
+            }
+        } catch (FileNotFoundException e) {
+            try {
+                FileWriter fileWriter = new FileWriter("Catalog.json");
+                fileWriter.write(jsonObject.toJSONString());
+                fileWriter.close();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+
         int portNumber = 1234; // replace with your port number
 
         try (
